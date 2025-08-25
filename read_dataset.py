e3e43a5c314dd91138b8b065790495ec7a3b646d
Ramprasath-0103 <ramprasath.rpurusothaman@gmail.com>
2025-08-25 21:44:52 +0530

    feat: addition of dataset into a dedicated python file ploted with connections

---
+++ read_dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd, networkx as nx, matplotlib.pyplot as plt
+
+df = pd.read_csv("indian_names_network_dataset.csv")
+G=nx.DiGraph()
+for _,r in df.iterrows():
+    G.add_edge(r['source'],r['target'],weight=r['weight'])
+
+print("Nodes:" ,G.number_of_nodes(),"Edges: ",G.number_of_edges())
+
+pos = nx.draw_spring(G,with_labels=True,node_size=500)
+plt.show()
